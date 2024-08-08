8a1b707d83ebe7d5416aece3cf832027a9705514
Juli <arevalojuli2@gmail.com>
2024-08-08 12:24:04 -0300

    Create julio-m4.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/julio-m4.py
@@ -0,0 +1,27 @@
+#https://python.org
+#Comentario de una linea
+
+...
+Otro comentario
+en varias
+lineas
+...
+
+" " "
+comentario dos 
+en varias
+lineas
+" " "
+
+
+variable = 'programar'
+MY_CONSTANTE = "palabrados"
+
+
+numero_int = 1
+numero_float = 3.5
+dat_bool = True
+dat_string = 'Mi primera activiad'
+
+
+print ('¡Hola, Python!')
